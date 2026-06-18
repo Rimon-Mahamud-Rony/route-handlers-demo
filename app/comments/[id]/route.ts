2c18d5dff5dfd3bec276b798daf3de8b390b4921
Rimon Mahamud Rony <rimonronycste11@gmail.com>
2026-06-18 12:11:45 +0600

    ci: handling patch routes

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -7,4 +7,20 @@ export async function GET(
     const { id } = await params;
     const comment = comments.find((c) => c.id === parseInt(id));
     return Response.json(comment);
+}
+
+export async function PATCH(
+    my_request: Request,
+    { params} : {params: Promise<{id: string}>}
+) {
+    const { id } = await params;
+    const body = await my_request.json();
+    const {text} = body;
+    const index = comments.findIndex((c) => c.id === parseInt(id));
+    if (index !== -1) {
+        comments[index].text = text;
+        return Response.json(comments[index]);
+    } else {
+        return new Response("Comment not found", { status: 404 });
     }
+}
